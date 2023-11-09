005be3aa9d2ce201f9d7abe693cd88963f74919f
DAON <diaeiyo1213@naver.com>
2023-11-09 11:26:47 +0900

    💡 - Getter / Setter를 이용한 CustomText 리소스 설정

--- app/src/main/java/com/daon/customtext/MainActivity.kt
+++ app/src/main/java/com/daon/customtext/MainActivity.kt
@@ -12,12 +12,44 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // CustomText 커스텀 뷰를 가져와 사용할 수 있습니다.
         val customText1 = binding.customText1
-
         val customText2 = binding.customText2
-
         val customText3 = binding.customText3
 
+        // mainTextViewText 및 subTextViewText를 사용하여 텍스트 설정
+        customText1.mainTextViewText = "이 동물을 소개합니다!"
+        customText1.subTextViewText = "- 기니피그"
+
+        // mainTextViewText 및 subTextViewText를 사용하여 텍스트 가져오기
+        customText1.mainTextViewText
+        customText1.subTextViewText
+
+        // ImageView에 이미지 리소스 설정/
+        customText1.setImageViewResource(R.drawable.ic_guinea)
+
+        // ImageView의 이미지 리소스 가져오기
+        customText1.imageViewResource
+
+        customText2.mainTextViewText = "이 동물을 소개합니다!"
+        customText2.subTextViewText = "- 판다"
+
+        customText2.mainTextViewText
+        customText2.subTextViewText
+
+        customText2.setImageViewResource(R.drawable.ic_panda)
+
+        customText2.imageViewResource
+
+        customText3.mainTextViewText = "이 동물을 소개합니다!"
+        customText3.subTextViewText = "- 레서판다"
+
+        customText3.mainTextViewText
+        customText3.subTextViewText
+
+        customText3.setImageViewResource(R.drawable.ic_lesser)
+
+        customText3.imageViewResource
     }
 }
+
+
